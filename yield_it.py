a61ba089e50462349a07c53844e025fd99310225
Doc Jones <doc.jones@tutanota.com>
2021-08-26 03:31:30 -0400

    Add Scheduler and create a simple round robin genertor

--- yield_it.py
+++ yield_it.py
@@ -1,4 +1,24 @@
 import time
+from collections import deque
+
+class Scheduler:
+    def __init__(self):
+        self.ready = deque()
+        self.current = None            # Currently excuting generator
+
+    def new_task(self, gen):
+        self.ready.append(gen)
+
+    def run(self):
+        while self.ready:
+            self.current = self.ready.popleft()
+            # Drive as a generator
+            try:
+                next(self.current)
+                if self.current:
+                    self.ready.append(self.current)
+            except StopIteration:
+                pass
 
 def countdown(n):
     while n > 0:
