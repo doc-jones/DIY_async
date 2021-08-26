3af9eb82de36dc089fc94599a6dc1c7c92e699ef
Doc Jones <doc.jones@tutanota.com>
2021-08-26 07:06:05 -0400

    Add producer.py starter code to async-await_producer.py

---
+++ async-await_producer.py
@@ -0,0 +1,30 @@
+# async-await_producer.py
+#
+
+import queue
+import threading
+import time
+
+
+def producer(q, count):
+    for n in range(count):
+        print('Producing', n)
+        q.put(n)
+        time.sleep(1)
+
+    print("Producer done")
+    q.put(None)  # 'sentinel' to shut down
+
+
+def consumer(q):
+    while True:
+        item = q.get()
+        if item is None:
+            break
+        print('Consuming', item)
+    print('Consumer done')
+
+
+q = queue.Queue()    # Thread safe queue
+threading.Thread(target=producer, args=(q, 10)).start()
+threading.Thread(target=consumer, args=(q,)).start()
