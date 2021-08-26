3d819db29689e5293f68def42a050869f75bd957
Doc Jones <doc.jones@tutanota.com>
2021-08-26 06:50:54 -0400

    Replace await switch with await sched, add deadline time man to run()

--- yield_it.py
+++ yield_it.py
@@ -1,11 +1,31 @@
 import time
 from collections import deque
+import heapq
+
+
+class Awaitable:
+    def __await__(self):
+        yield
+
+
+def switch():
+    return Awaitable()
 
 
 class Scheduler:
     def __init__(self):
         self.ready = deque()  # Async queue
+        self.sleeping = []
         self.current = None  # Currently executing generator
+        self.sequence = 0
+
+    async def sleep(self, delay):
+        # The current coroutine/task wants to sleep
+        deadline = time.time() + delay
+        self.sequence += 1
+        heapq.heappush(self.sleeping, (deadline, self.sequence, self.current))
+        self.current = None
+        await switch()          # Switch tasks
 
     # def new_task(self, gen):
     #     self.ready.append(gen)
@@ -13,7 +33,14 @@ class Scheduler:
         self.ready.append(coro)
 
     def run(self):
-        while self.ready:
+        while self.ready or self.sleeping:
+            if not self.ready:             # time management exactly like the callback example.
+                deadline, _, coro = heapq.heappop(self.sleeping)
+                delta = deadline - time.time()
+                if delta > 0:
+                    time.sleep(delta)
+                self.ready.append(coro)
+
             self.current = self.ready.popleft()
             # Drive as a generator
             try:
@@ -27,21 +54,13 @@ class Scheduler:
 sched = Scheduler()  # Background scheduler object
 
 
-class Awaitable:
-    def __await__(self):
-        yield
-
-
-def switch():
-    return Awaitable()
-
-
 async def countdown(n):              # keyword async require to use await
     while n > 0:
         print('Down', n)
-        time.sleep(1)
+        # time.sleep(1)
+        await sched.sleep(4)  # Takes care of switching tasks
         # yield
-        await switch()  # Switch tasks
+        # await switch()  # Switch tasks
         n -= 1
 
 
@@ -49,14 +68,15 @@ async def countup(stop):    # keyword async require to use await
     x = 0
     while x < stop:
         print('Up', x)
-        time.sleep(1)
+        # time.sleep(1)
+        await sched.sleep(1)  # Takes care of switching tasks
         # yield
-        await switch()      # Switch tasks
+        # await switch()      # Switch tasks
         x += 1
 
 
 sched.new_task(countdown(5))
-sched.new_task(countup(5))
+sched.new_task(countup(20))
 sched.run()
 
 # Simplifies code over using callbacks: no helper func needed, no recursion needed
