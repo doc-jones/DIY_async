b81a9456d274435d8a462535501139f79c16744b
Doc Jones <doc.jones@tutanota.com>
2021-08-26 07:13:29 -0400

    add the Scheduler, sleep and run methods from yield_it, come comments

--- async-await_producer.py
+++ async-await_producer.py
@@ -1,9 +1,63 @@
 # async-await_producer.py
-#
+# using the same scheduler, sleep and run methods as from yield_it.py
+# async-await in python is just the masking of the yield cmd.
 
-import queue
-import threading
 import time
+from collections import deque
+import heapq
+
+
+class Awaitable:
+    def __await__(self):
+        yield
+
+
+def switch():
+    return Awaitable()
+
+
+class Scheduler:
+    def __init__(self):
+        self.ready = deque()  # Async queue
+        self.sleeping = []
+        self.current = None  # Currently executing generator
+        self.sequence = 0
+
+    async def sleep(self, delay):
+        # The current coroutine/task wants to sleep
+        deadline = time.time() + delay
+        self.sequence += 1
+        heapq.heappush(self.sleeping, (deadline, self.sequence, self.current))
+        self.current = None
+        await switch()          # Switch tasks
+
+    # def new_task(self, gen):
+    #     self.ready.append(gen)
+    def new_task(self, coro):
+        self.ready.append(coro)
+
+    def run(self):
+        while self.ready or self.sleeping:
+            if not self.ready:             # time management exactly like the callback example.
+                deadline, _, coro = heapq.heappop(self.sleeping)
+                delta = deadline - time.time()
+                if delta > 0:
+                    time.sleep(delta)
+                self.ready.append(coro)
+
+            self.current = self.ready.popleft()
+            # Drive as a generator
+            try:
+                self.current.send(None)   # next(self.current)  # Together with yield this replaces callbacks in driving the code
+                if self.current:
+                    self.ready.append(self.current)
+            except StopIteration:
+                pass
+
+
+sched = Scheduler()  # Background scheduler object
+
+#  --------------------------------------------------------------
 
 
 def producer(q, count):
